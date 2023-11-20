4d3782484358a1ea34aaf50acce111d0383f9544
DaffafauzanD <daffafauzand24@gmail.com>
2023-11-21 01:42:11 +0700

    Upload Bookshelf API

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const {
+  addBookHandler,
+  getAllBookHandler,
+  getBooksByIdHandler,
+  editBookByIdHandler,
+  deleteBooksByIdhandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBooksByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBooksByIdhandler,
+  },  
+];
+
+module.exports = routes;
